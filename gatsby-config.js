d56afe545f355b18f0e46d1a068bd0ad777fca5e
ilman <ilman1990@gmail.com>
2021-01-01 23:07:13 +0700

    trying gatsby, query for local files

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,24 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Pandas that eat a lot`,
+  },
+  plugins: [
+    {
+      resolve:`gatsby-source-filesystem`,
+      options:{
+        name:`src`,
+        path:`${__dirname}/src/`
+      }
+    },
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
